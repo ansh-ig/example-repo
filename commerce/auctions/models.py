3462b6e6cec750a0024e36a03a49415dcf91ef82
ansh-ig <hellowsam1492@gmail.com>
2024-12-03 13:26:20 +0530

    added bunch of functions , recently one was a adding/removing to  watchlist

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,6 +19,8 @@ class Listing(models.Model):
     isActive = models.BooleanField(default=True)
     owner = models.ForeignKey (User, on_delete=models.CASCADE, related_name="user")
     category = models.ForeignKey (Category, on_delete=models.CASCADE, blank=True, related_name="category")
+    watchlist = models.ManyToManyField( User, blank=True, null=True, related_name="listingwatchlist")
+
 
     def __str__(self):
         return self.tittle
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -9,8 +9,10 @@ from .models import User, Category, Listing
 
 def index(request):
     activeListings = Listing.objects.filter (isActive=True)
+    allCategories = Category.objects.all()
     return render(request, "auctions/index.html", {
-        "listings": activeListings
+        "listings": activeListings,
+        "categories": allCategories
     })
 
 def createListing(request):
@@ -43,8 +45,40 @@ def createListing(request):
         newListing.save()
         # Redirect to index page
         return HttpResponseRedirect(reverse(index))
+    
+def displayCategory(request):
+    if request.method == "POST":
+        formcategory = request.POST['category']
+        category = Category.objects.get(categoryName=formcategory)
+        activeListings = Listing.objects.filter (isActive=True, category=category)
+        allCategories = Category.objects.all()
+        return render(request, "auctions/index.html", {
+            "listings": activeListings,
+            "categories": allCategories
+    })
 
+def listing(request, id):
+    listingInfo = Listing.objects.get(pk=id)
+    isListingInWatchlist = request.user in listingInfo.watchlist.all()
+    return render(request, "auctions/listing.html", {
+        "listing": listingInfo,
+        "isListingInWatchlist": isListingInWatchlist
+    })
 
+def displayWatchlist(request):
+    return
+
+def removeWatchlist(request,id):
+    listingInfo = Listing.objects.get(pk=id)
+    usernow = request.user
+    listingInfo.watchlist.remove(usernow)
+    return HttpResponseRedirect(reverse("listing", args=(id, )))
+
+def addWatchlist(request,id):
+    listingInfo = Listing.objects.get(pk=id)
+    usernow = request.user
+    listingInfo.watchlist.add(usernow)
+    return HttpResponseRedirect(reverse("listing", args=(id, )))
 
 def login_view(request):
     if request.method == "POST":
